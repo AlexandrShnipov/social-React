a981fbc23717fd8045085a97722e36457cbb9bd3
AlexandrShnipov <sshnipov@gmail.com>
2022-07-12 22:30:09 +0300

    refactor Preloader.jsx

--- src/components/Common/Preloader/Preloader.jsx
+++ src/components/Common/Preloader/Preloader.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import s from "./Preloader.module.css";
-import preloader from "../../../assets/images/preloader.svg";
 
 const Preloader = (props) => {
     return (
         <div className={s.isLoadingContainer}>
-            <img src={preloader}/>
+            <div></div>
         </div>
     )
 }
