0e62df4b0a6701bfcc7c81683f3a9f8feb5267c4
Alexandr <sshnipov@gmail.com>
2022-08-24 20:57:24 +0400

    added ReduxDev Tools in reduxStore.jsx

--- src/redux/reduxStore.jsx
+++ src/redux/reduxStore.jsx
@@ -19,6 +19,7 @@ let reducers = combineReducers({
   app: appReducer
 });
 
+ //added REDUX_DEVTOOLS
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMidleware)));
 
