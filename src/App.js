7e1996e6a953c058134f045c0385c71112fa054a
Alexandr <sshnipov@gmail.com>
2022-09-22 13:10:54 +0400

    Revert "added react.lazy in APP"
    This reverts commit e2e54a400b4bcdc03cb7c53b4254ca661c772204.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import './App.css';
 import './Reset.css';
+import News from "./components/news/News";
+import Music from "./components/music/Music";
+import Setting from "./components/setting/Setting";
 import {Route, Routes} from "react-router-dom";
+import Friends from "./components/friends/Friends";
+import DialogsContainer from "./components/dialogs/DialogsContainer";
 import NavBarContainer from "./components/navBar/NavBarContainer";
 import UsersContainer from "./components/users/UsersContainer";
-import {withRouter} from "./components/profile/ProfileContainer";
+import ProfileContainer, {withRouter} from "./components/profile/ProfileContainer";
 import HeaderContainer from "./components/header/HeaderContainer";
+import Login from "./components/login/Login";
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/appReduser";
 import Preloader from "./components/Common/Preloader/Preloader";
-const ProfileContainer = React.lazy(() => import('./components/profile/ProfileContainer'));
-const DialogsContainer = React.lazy(() => import('./components/dialogs/DialogsContainer'));
-const News = React.lazy(() => import('./components/news/News'));
-const Music = React.lazy(() => import('./components/music/Music'));
-const Setting = React.lazy(() => import('./components/setting/Setting'));
-const Friends = React.lazy(() => import('./components/friends/Friends'));
-const Login = React.lazy(() => import('./components/login/Login'));
 
 class App extends Component {
 
@@ -37,24 +36,24 @@ class App extends Component {
         <HeaderContainer/>
         <NavBarContainer/>
         <main className={'contentMain'}>
-          <React.Suspense fallback={<Preloader/>}>
-            <Routes>
-              <Route path='/profile' element={<ProfileContainer/>}/>
-              <Route path='/profile/:userId'
-                     element={<ProfileContainer/>}
-              />
-              <Route path='/dialogs/*'
-                     element={ <DialogsContainer/>}
-              />
-              <Route path='/news/*' element={<News/>}/>
-              <Route path='/music/*' element={<Music/>}/>
-              <Route path='/users/*' element={<UsersContainer/>}/>
-              <Route path='/setting/*' element={<Setting/>}/>
-              <Route path='/friends/*' element={<Friends/>}/>
-              <Route path='/login/*' element={<Login/>}/>
-            </Routes>
-          </React.Suspense>
-
+          <Routes>
+            <Route path='/profile' element={<ProfileContainer/>}/>
+            <Route path='/profile/:userId'
+                   element={<ProfileContainer/>}
+            />
+            <Route path='/dialogs/*'
+                   element={<DialogsContainer
+                     // stateDialogsPage={props.state.dialogsPage}
+                     // dispatch={props.dispatch}
+                   />}
+            />
+            <Route path='/news/*' element={<News/>}/>
+            <Route path='/music/*' element={<Music/>}/>
+            <Route path='/users/*' element={<UsersContainer/>}/>
+            <Route path='/setting/*' element={<Setting/>}/>
+            <Route path='/friends/*' element={<Friends/>}/>
+            <Route path='/login/*' element={<Login/>}/>
+          </Routes>
         </main>
       </div>
     );
