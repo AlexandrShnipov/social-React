cd063efac01d522855aa9b962ab5fdfba3413501
AlexandrShnipov <sshnipov@gmail.com>
2022-10-20 19:57:13 +0300

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
     root.render(
         <React.StrictMode>
-            <BrowserRouter basename={process.env.PUBLIC_URL}>
+            <BrowserRouter basename='/social-React'>
                 <Provider store = {store}>
                     <App/>
                 </Provider>
